a84b12394d8acdb2123bbe8cf849ad93fe2e7a23
Your Name <you@example.com>
2020-03-31 10:00:45 -0700

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  get '/' do
+    erb :index
+    # tells the application to render, or deliver to the users browser, the file in views/index.erb
+  end
+
+  post '/greet' do
+    erb :greet
+  end
 
 end
